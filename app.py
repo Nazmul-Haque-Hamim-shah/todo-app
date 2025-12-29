d9f3eac5873c708bc5d78ec8a52a6bc693b97345
Nazmul Haque Hamim shah <shahit655@gmail.com>
2025-12-30 01:41:35 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+
+from flask import Flask,render_template,url_for,request,redirect
+from db import db,Tasks,User
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://postgres:yQDdmbNzfoZWxJMsFRgwWabtNXsQROON@centerbeam.proxy.rlwy.net:46372/railway"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def index():
+    tasks=Tasks.query.all()
+    return render_template("index.html",tasks=tasks)
+
+
+@app.route("/add",methods=["POST","GET"])
+def add():
+    if request.method=="POST":
+        task=request.form.get("task")
+        new_task=Tasks(text=task)
+        db.session.add(new_task)
+        db.session.commit()
+
+        return redirect(url_for("index"))
+
+
+
+
+@app.route("/update/<int:id>",methods=["POST","GET"])
+def update(id):
+    task=Tasks.query.get_or_404(id)
+    if request.method =="POST":
+        task.text=request.form.get("task")
+        db.session.commit()
+        return redirect(url_for("index"))
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task = Tasks.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+
---
+++ db.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+
+class Tasks(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    text=db.Column(db.String(100),nullable=False)
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(50),unique=True,nullable=False)
+    email=db.Column(db.String(50),unique=True,nullable=False)
+    password=db.Column(db.String(10),nullable=False)
