933a1b949c286a2013b15070dd334b1f850411d3
Nazmul Haque Hamim shah <shahit655@gmail.com>
2025-12-30 19:10:12 +0600

    Update db.py

--- db.py
+++ db.py
@@ -8,6 +8,6 @@ class Tasks(db.Model):
 
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(50),unique=True,nullable=False)
-    email=db.Column(db.String(50),unique=True,nullable=False)
-    password=db.Column(db.String(10),nullable=False)
+    name=db.Column(db.String(200),unique=True,nullable=False)
+    email=db.Column(db.String(200),unique=True,nullable=False)
+    password=db.Column(db.String(500),nullable=False)
